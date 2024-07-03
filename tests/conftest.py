ba0a9b50825c374baff35f6f7e1b093b1c59c5cb
gonzalrj <rjgonzalez@live.com>
2024-07-04 00:08:52 +0800

    Updated browser options.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,31 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
 
 
 @pytest.fixture()
 # This function runs test based on command-line provided browser option
 def driver(request):
     browser = request.config.getoption("--browser")
-    options = Options()
-    options.add_argument("--headless")
     print(f"Creating {browser} driver.")
 
     # Create the browser instance based on the inputted --browser value from def pytest_addoption(parser)
     if browser == "chrome":
+        options = ChromeOptions()
+        options.add_argument("--headless")
         my_driver = webdriver.Chrome(options=options)
     elif browser == "firefox":
+        options = FirefoxOptions()
+        options.add_argument("--headless")
         my_driver = webdriver.Firefox(options=options)
     elif browser == "edge":
+        options = EdgeOptions()
+        options.add_argument("--headless")
         my_driver = webdriver.Edge(options=options)
     elif browser == "safari":
-        my_driver = webdriver.Safari(options=options)
+        my_driver = webdriver.Safari()  # Safari does not support headless execution.
     else:
         raise TypeError(f"Expected 'chrome' or 'firefox' or 'edge' or 'safari' but got {browser} instead.")
 
